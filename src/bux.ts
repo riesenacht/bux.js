c44da67a2dad27f343fd82bded526c17ef7a4b8c
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2022-05-19 17:19:56 +0200

    add demo/test page

--- src/bux.ts
+++ src/bux.ts
@@ -1,1 +1,14 @@
 console.log("bux.js loaded");
+
+onReady(() => {
+    let inputs = document.querySelectorAll("input");
+    console.log("inputs", inputs);
+});
+
+function onReady(fn: EventListener) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+        return;
+    }
+    document.addEventListener("DOMContentLoaded", fn);
+}

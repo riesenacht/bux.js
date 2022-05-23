47bc6541fc0cace8b1ea8857f3ab7a62aa748573
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2022-05-23 19:08:50 +0200

    add possibility to use min/max on phone input

--- src/model/PhoneInput.ts
+++ src/model/PhoneInput.ts
@@ -9,8 +9,9 @@ export class PhoneInput extends BasicInput {
         let phoneInput = document.createElement("input");
         phoneInput.classList.add("bux-input");
         phoneInput.type = "range";
-        phoneInput.min = "0100000000";
-        phoneInput.max = "9999999999";
+        phoneInput.min = this.getElement().min != "" ? this.getElement().min : "0100000000";
+        phoneInput.max = this.getElement().max != "" ? this.getElement().max : "9999999999";
+        
         this.setElement(phoneInput);
 
         this.output.classList.add("html-input-output");
@@ -29,7 +30,7 @@ export class PhoneInput extends BasicInput {
     }
 
     updateOutput() {
-        this.output.textContent = this.getElement().value;
+        this.output.textContent = this.getElement().value.length < 10 ? "0" + this.getElement().value : this.getElement().value;
     }
 
     onChange() {

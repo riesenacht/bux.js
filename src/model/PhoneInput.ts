aa58c82ba0cfb89a6e8c8c8f184ac84b2f52fc9c
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2022-06-10 18:24:37 +0200

    add a way to fine adjust the phone number

--- src/model/PhoneInput.ts
+++ src/model/PhoneInput.ts
@@ -6,9 +6,32 @@ export class PhoneInput extends BasicInput {
     getNewHtml(): HTMLElement {
         let div = document.createElement("div");
 
+        let subtractButton = document.createElement("button");
+        subtractButton.textContent = "-";
+        subtractButton.addEventListener("click", (event: Event) => {
+            event.preventDefault();
+
+            if(parseInt(phoneInput.value) > parseInt(phoneInput.min)) {
+                phoneInput.value = "" + (parseInt(phoneInput.value) - 1);
+                this.updateOutput();
+            }
+        });
+        
+        let addButton = document.createElement("button");
+        addButton.textContent = "+";
+        addButton.addEventListener("click", (event: Event) => {
+            event.preventDefault();
+
+            if(parseInt(phoneInput.value) < parseInt(phoneInput.max)) {
+                phoneInput.value = "" + (parseInt(phoneInput.value) + 1);
+                this.updateOutput();
+            }
+        })
+
         let phoneInput = document.createElement("input");
         phoneInput.classList.add("bux-input");
         phoneInput.type = "range";
+        phoneInput.step = "1";
         phoneInput.min = this.getElement().min != "" ? this.getElement().min : "0100000000";
         phoneInput.max = this.getElement().max != "" ? this.getElement().max : "9999999999";
         
@@ -16,8 +39,11 @@ export class PhoneInput extends BasicInput {
 
         this.output.classList.add("html-input-output");
 
+        div.append(subtractButton);
         div.append(phoneInput);
+        div.append(addButton);
         div.append(this.output);
+        this.getElement().value = this.getElement().min;
         this.updateOutput();
 
         return div;
